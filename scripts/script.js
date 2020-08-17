73df8c2b4e310de78bf73770480b3c0b0b4c8ab3
Lina <z-lina@mail.ru>
2020-08-18 01:14:05 +0300

    search

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const dataBase = JSON.parse(localStorage.getItem('awito')) || [];
+let counter = dataBase.length;
 const modalAdd = document.querySelector('.modal__add');
 const addAd = document.querySelector('.add__ad');
 const modalBtnSubmit = document.querySelector('.modal__btn-submit');
@@ -13,6 +14,13 @@ const modalFileBtn = document.querySelector('.modal__file-btn');
 const modalImageAdd = document.querySelector('.modal__image-add');
 const textFileBtn = modalFileBtn.textContent;
 const srcModalImage = modalImageAdd.src;
+const modalImageItem = document.querySelector('.modal__image-item');
+const modalHeaderItem = document.querySelector('.modal__header-item');
+const modalStatusItem = document.querySelector('.modal__status-item');
+const modalDescriptionItem = document.querySelector('.modal__description-item');
+const modalCostItem = document.querySelector('.modal__cost-item');
+const searchInput = document.querySelector('.search__input');
+const menuContainer = document.querySelector('.menu__container');
 
 const elementsModalSubmit = [...modalSubmit.elements]
   .filter(elem => elem.tagName !== 'BUTTON');
@@ -33,21 +41,22 @@ const closeModal = function(event) {
   if (target.closest('.modal__close') || 
       target.classList.contains('modal') ||
       event.code === 'Escape') {
-        modalAdd.classList.add('hide');
-        modalItem.classList.add('hide');
-        document.removeEventListener('keydown', closeModal);
-        modalSubmit.reset();
+        modalAdd.classList.add('hide'); 
+        modalItem.classList.add('hide'); 
+        document.removeEventListener('keydown', closeModal); 
+        modalSubmit.reset(); 
         modalImageAdd.src = srcModalImage;
         modalFileBtn.textContent = textFileBtn;
         checkForm();
       }
 }; 
 
-const renderCard = () => {
+const renderCard = (DB = dataBase) => {
   catalog.textContent = '';
-  dataBase.forEach((item, i) => {
+
+  DB.forEach(item => {
     catalog.insertAdjacentHTML('beforeend', `
-      <li class="card" data-id="${i}">
+      <li class="card" data-id="${item.id}">
 		  	<img class="card__image" src="data:image/jpeg;base64,${item.image}" alt="test">
 			  <div class="card__description">
 			  	<h3 class="card__header">${item.nameItem}</h3>
@@ -58,11 +67,21 @@ const renderCard = () => {
   });
 };
 
+searchInput.addEventListener('input', () => {
+  const valueSearch = searchInput.value.trim().toLowerCase();
+
+  if(valueSearch.length > 2) {
+    const result = dataBase.filter(item => item.nameItem.toLowerCase().includes(valueSearch) ||
+                                          item.descriptionItem.toLowerCase().includes(valueSearch));
+    renderCard(result);
+  }
+});
+
 modalFileInput.addEventListener('change', event => {
   const target = event.target;
   const reader = new FileReader();
-
   const file = target.files[0];
+
   infoPhoto.filename = file.name;
   infoPhoto.size = file.size;
   
@@ -88,6 +107,7 @@ modalSubmit.addEventListener('submit', event => {
   for (const elem of elementsModalSubmit) {
     itemObj[elem.name] = elem.value;
   } 
+  itemObj.id = counter++;
   itemObj.image = infoPhoto.base64;
   dataBase.push(itemObj);
   closeModal({target: modalAdd});
@@ -103,9 +123,26 @@ addAd.addEventListener('click', () => {
 
 catalog.addEventListener('click', event => {
   const target = event.target;  
-  if(target.closest('.card')) {
+  const card = target.closest('.card');
+  if(card) { 
+    const item = dataBase.find(obj => obj.id === parseInt(card.dataset.id));
+
+    modalImageItem.src = `data:image/jpeg;base64,${item.image}`;
+    modalHeaderItem.textContent = item.nameItem;
+    modalStatusItem.textContent = item.status === 'new' ? 'Новый' : 'Б/У';
+    modalDescriptionItem.textContent = item.descriptionItem;
+    modalCostItem.textContent = item.costItem;
     modalItem.classList.remove('hide');
     document.addEventListener('keydown', closeModal);
+  }
+});
+
+menuContainer.addEventListener('click', event => {
+  const target = event.target;
+
+  if(target.tagName === 'A') {
+    const result = dataBase.filter(item => item.category === target.dataset.category);
+    renderCard(result);
   }
 });
 
